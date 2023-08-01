1fe389552f283357a4f20858a02132efad41d688
Sukhpreet2001 <kaurcs2001@gmail.com>
2023-08-01 12:13:05 +0530

    update

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const diaryList = document.getElementById('diary-list');
 
 const dailyDiaryEntries = [
-  { date: '2023-07-11', title: 'Day 1', content: 'Introduction to DevOps.' },
-  { date: '2023-07-12', title: 'Day 2', content: 'Introduction to GitHub.' },
+  { date: '2023-07-11', title: 'Day 1' },
+  { date: '2023-07-12', title: 'Day 2' },
   // Add more daily diary entries here...
 ];
 
 function createDiaryEntryLink(entry) {
   const listItem = document.createElement('li');
   const link = document.createElement('a');
-  link.href = `./entries/${entry.date}.md`; // Assuming the daily diary entries are in the 'entries' directory
+  link.href = `./entries/${entry.date}.md`;
   link.textContent = entry.title;
   listItem.appendChild(link);
   diaryList.appendChild(listItem);
+
+  // Fetch the content of the Markdown file and render it as HTML
+  fetch(`./entries/${entry.date}.md`)
+    .then((response) => response.text())
+    .then((markdownContent) => {
+      const entryContent = document.createElement('div');
+      entryContent.innerHTML = marked(markdownContent);
+      listItem.appendChild(entryContent);
+    })
+    .catch((error) => {
+      console.error(`Error fetching or rendering ${entry.date}.md`, error);
+    });
 }
 
 function populateDiaryList() {
