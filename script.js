b0161dbba07174347d06b68c7b1a1f5253bc9a05
Sukhpreet2001 <kaurcs2001@gmail.com>
2023-08-01 12:17:37 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const diaryList = document.getElementById('diary-list');
 
 const dailyDiaryEntries = [
-  { date: '2023-07-11', title: 'Day 1' },
-  { date: '2023-07-12', title: 'Day 2' },
+  { date: '2023-07-11', title: 'Day 1', content: 'Introduction to DevOps.' },
+  { date: '2023-07-12', title: 'Day 2', content: 'Introduction to GitHub.' },
   // Add more daily diary entries here...
 ];
 
@@ -12,6 +12,12 @@ function createDiaryEntryLink(entry) {
   link.href = `./entries/${entry.date}.md`;
   link.textContent = entry.title;
   listItem.appendChild(link);
+
+  // Use the content property as the summary
+  const summary = document.createElement('p');
+  summary.textContent = entry.content;
+  listItem.appendChild(summary);
+
   diaryList.appendChild(listItem);
 
   // Fetch the content of the Markdown file and render it as HTML
