f255060e30caa27be1e4f93136166abcc844655d
Sukhpreet2001 <kaurcs2001@gmail.com>
2023-08-06 22:58:12 +0530

    update diary

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ const dailyDiaryEntries = [
   { date: '2023-07-12', title: 'Day 2', content: `Introduction to GitHub` },
   { date: '2023-07-13', title: 'Day 3', content: `Making GitHub Pages` },
   { date: '2023-07-14', title: 'Day 4', content: `Introduction to Docker` },
-  { date: '2023-07-13', title: 'Day 5', content: `Learning about Dockerfile` },
+  { date: '2023-07-17', title: 'Day 5', content: `Learning about Dockerfile` },
+  { date: '2023-07-18', title: 'Day 6', content: `Project Assignment` },
+  { date: '2023-07-19', title: 'Day 7', content: `Linux with Docker` },
+  { date: '2023-07-20', title: 'Day 8', content: `Dockerise Project:Recruitement Project` },
+  { date: '2023-07-21', title: 'Day 9', content: `Solving Errors` },
+  { date: '2023-07-24', title: 'Day 10', content: `Presentation Day` },
+  { date: '2023-07-25', title: 'Day 11', content: `Introsduction to Google Webmaster` },
+  { date: '2023-07-26', title: 'Day 12', content: `Introduction to Jenkins` },
+  { date: '2023-07-27', title: 'Day 13', content: `Jenkins Setup` },
+  { date: '2023-07-28', title: 'Day 14', content: `CI/CD Pipeline using Jenkins` },
   // Add more daily diary entries here...
 ];
 
